68db5df16a152d7281db9123c6aa1cf0a0ed45fd
Nuriage <javascript@ZiMac05.local>
2016-06-23 21:51:11 +0200

    Ejercicios clase 4

---
+++ Nuriage/Pública/operaciones.js
@@ -0,0 +1,19 @@
+var numeroa = 5;
+var numerob = 40;
+var resultado = (numeroa+numerob);
+console.log("El resultado es ", resultado);
+
+var numeroa = 5;
+var numerob = 40;
+var resultado = (numerob-numeroa);
+console.log("El resultado es ", resultado);
+
+var numeroa = 5;
+var numerob = 40;
+var resultado = (numerob/numeroa);
+console.log("El resultado es ", resultado);
+
+var numeroa = 5;
+var numerob = 40;
+var resultado = (numerob*numeroa);
+console.log("El resultado es ", resultado);
---
+++ Nuriage/Pública/porcentajes.js
@@ -0,0 +1,8 @@
+var hombres = 6;
+var mujeres = 2;
+
+var resultadom = (mujeres*100)/8
+console.log("El porcentaje total de mujeres es", resultadom)
+
+var resultadoh = (hombres*100)/8
+console.log("El porcentaje total de hombres es", resultadoh)
