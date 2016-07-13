5f089ffdde751b2ef1b7d2c72f42699077449cab
Nuria Hill <nuria.hilligehekken@gmail.com>
2016-07-13 11:00:46 +0000

    added file for step 2 in vendor machine practice to folder clase9

---
+++ NuriaH/public/Clase9/M_EX_2.js
@@ -0,0 +1,23 @@
+/*Paso 2 - Creamos los primeros perfiles en el Array de clientes:
+
+Propiedades:
+Nombre
+Usuario
+Contraseña
+Tipo usuario
+Presupuesto*/
+//aqui solo modificaremos el array de clientes dejando el resto como esta
+var MaquinaExpendedora = {};
+var Productos = [];
+
+// dentro del array clientes, como hay propiedades creamos un objeto
+var Clientes = [{Nombre:"Administrador",
+                 Usuario: "userid",
+                 Contraseña: "password",
+                 Tipousuario: "Administrador",
+                 Presupuesto : 1000},
+                {Nombre:"Usuario",
+                 Usuario: "userid",
+                 Contraseña: "password",
+                 Tipousuario: "Usuario",
+                 Presupuesto : 1000}];
